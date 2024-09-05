b4742a90e7e79b2d969a0cd801d587ff25d4d45b
Deka Jello <dekarrin.irl@gmail.com>
2024-09-05 07:45:13 -0500

    okay everyfin is at least encapsulated in lib if hitting db

--- mtg/db/carddb.py
+++ mtg/db/carddb.py
@@ -1,8 +1,24 @@
 import sqlite3
 import sys
 
-from . import util
+from . import util, editiondb
 from .. import cio, cardutil
+
+
+def get_all(db_filename):
+	con = util.connect(db_filename)
+	cur = con.cursor()
+	
+	data = list()
+	
+	for r in cur.execute(sql_get_all_cards):
+		data_dict = 
+		
+		data.append(data_dict)
+		
+	con.close()
+	
+	return data
 
 
 def get_one(db_filename, cid):
@@ -11,24 +27,7 @@ def get_one(db_filename, cid):
 	
 	rows = []
 	for r in cur.execute(sql_find_card_by_id, (cid,)):
-		data_dict = {
-			'id': r[0],
-			'count': r[1],
-			'name': r[2],
-			'edition': r[3],
-			'tcg_num': r[4],
-			'condition': r[5],
-			'language': r[6],
-			'foil': util.int_to_bool(r[7]),
-			'signed': util.int_to_bool(r[8]),
-			'artist_proof': util.int_to_bool(r[9]),
-			'altered_art': util.int_to_bool(r[10]),
-			'misprint': util.int_to_bool(r[11]),
-			'promo': util.int_to_bool(r[12]),
-			'textless': util.int_to_bool(r[13]),
-			'printing_id': r[14],
-			'printing_note': util.none_to_empty_str(r[15]),
-		}
+		data_dict = _card_row_to_dict(r)
 		rows.append(data_dict)
 
 	count = len(rows)		
@@ -45,6 +44,7 @@ def get_one(db_filename, cid):
 	
 
 # TODO: do not put prompt into this, split prompting into own func.
+# TODO: unify interface with find_by_filter and editionsdb.find.
 def find_one_by_filter(db_filename, name, card_num):
 	con = util.connect(db_filename)
 	where_clause = ''
@@ -93,25 +93,7 @@ def find_one_by_filter(db_filename, name, card_num):
 	query = sql_select_cards + where_clause
 	
 	for r in cur.execute(query, params):
-		data_dict = {
-			'id': r[0],
-			'count': r[1],
-			'name': r[2],
-			'edition': r[3],
-			'tcg_num': r[4],
-			'condition': r[5],
-			'language': r[6],
-			'foil': util.int_to_bool(r[7]),
-			'signed': util.int_to_bool(r[8]),
-			'artist_proof': util.int_to_bool(r[9]),
-			'altered_art': util.int_to_bool(r[10]),
-			'misprint': util.int_to_bool(r[11]),
-			'promo': util.int_to_bool(r[12]),
-			'textless': util.int_to_bool(r[13]),
-			'printing_id': r[14],
-			'printing_note': util.none_to_empty_str(r[15]),
-		}
-		
+		data_dict = _card_row_to_dict(r)
 		data.append(data_dict)
 	
 	con.close()
@@ -135,32 +117,29 @@ def find_one_by_filter(db_filename, name, card_num):
 	return data[0]
 	
 
-def get_all(db_filename):
-	con = util.connect(db_filename)
+def find_by_filter(db_filename, filter_clause, filter_params):
+	try:
+		con = sqlite3.connect("file:" + db_filename + "?mode=rw", uri=True)
+	except sqlite3.OperationalError as e:
+		if (e.sqlite_errorcode & 0xff) == 0x0e:
+			print("ERROR: Cannot open DB file {!r}; does it exist?".format(db_filename), file=sys.stderr)
+		else:
+			print("ERROR: SQLITE returned an error opening DB: {:s}({:d})".format(e.sqlite_errorname, e.sqlite_errorcode), file=sys.stderr)
+		sys.exit(2)
+	
 	cur = con.cursor()
 	
 	data = list()
 	
-	for r in cur.execute(sql_get_all_cards):
-		data_dict = {
-			'id': r[0],
-			'count': r[1],
-			'name': r[2],
-			'edition': r[3],
-			'tcg_num': r[4],
-			'condition': r[5],
-			'language': r[6],
-			'foil': util.int_to_bool(r[7]),
-			'signed': util.int_to_bool(r[8]),
-			'artist_proof': util.int_to_bool(r[9]),
-			'altered_art': util.int_to_bool(r[10]),
-			'misprint': util.int_to_bool(r[11]),
-			'promo': util.int_to_bool(r[12]),
-			'textless': util.int_to_bool(r[13]),
-			'printing_id': r[14],
-			'printing_note': util.none_to_empty_str(r[15]),
-		}
-		
+	query = sql_select_cards
+	
+	params = list()
+	if filter_clause != '':
+		query += filter_clause
+		params += filter_params
+	
+	for r in cur.execute(query, params):
+		data_dict = _card_row_to_dict(r)
 		data.append(data_dict)
 		
 	con.close()
@@ -194,6 +173,90 @@ def update_counts(db_filename, cards):
 	cur.executemany(sql_update_count, update_data)
 	con.commit()
 	con.close()
+	
+
+def build_filters(db_filename, name=None, card_num=None, edition=None):
+	if name is None and card_num is None and edition is None:
+		return "", []
+		
+	clause = ' WHERE'
+	
+	num_exprs = 0
+	data_params = list()
+	
+	if name is not None:
+		clause += ' c.name LIKE "%" || ? || "%"'
+		num_exprs += 1
+		data_params.append(name)
+		
+	if card_num is not None:
+		ed = None
+		tcg_num = None
+		
+		splits = card_num.split('-', maxsplit=1)
+		if len(splits) == 2:
+			ed = splits[0]
+			
+			if splits[1] != '':
+				tcg_num = splits[1]
+		elif len(splits) == 1:
+			ed = splits[0]
+			
+		if ed is not None:
+			if num_exprs > 0:
+				clause += " AND"
+			clause += " c.edition = ?"
+			num_exprs += 1
+			data_params.append(ed)
+			
+		if tcg_num is not None:
+			if num_exprs > 0:
+				clause += " AND"
+			clause += " c.tcg_num = ?"
+			num_exprs += 1
+			data_params.append(tcg_num)
+		
+	if edition is not None:
+		# we need to look up editions first or we are going to need to do a dynamically built
+		# join and i dont want to
+		matching_editions = editionsdb.find(db_filename, edition)
+		
+		# match on any partial matches and get the codes
+		matched_codes = []
+		for ed in matching_editions:
+			matched_codes.append(ed['code'])
+		
+		if num_exprs > 0:
+			clause += " AND"
+		
+		matched_codes = ["'" + x + "'" for x in matched_codes]
+			
+		# no way to bind list values... but we got them from the DB, not user
+		# input, so we should just be able to directly add them safely.
+		clause += " c.edition IN (" + ','.join(matched_codes) + ")"
+		
+	return clause, data_params
+	
+
+def _card_row_to_dict(r):
+	return {
+		'id': r[0],
+		'count': r[1],
+		'name': r[2],
+		'edition': r[3],
+		'tcg_num': r[4],
+		'condition': r[5],
+		'language': r[6],
+		'foil': util.int_to_bool(r[7]),
+		'signed': util.int_to_bool(r[8]),
+		'artist_proof': util.int_to_bool(r[9]),
+		'altered_art': util.int_to_bool(r[10]),
+		'misprint': util.int_to_bool(r[11]),
+		'promo': util.int_to_bool(r[12]),
+		'textless': util.int_to_bool(r[13]),
+		'printing_id': r[14],
+		'printing_note': util.none_to_empty_str(r[15]),
+	}
 
 
 sql_find_card_by_id = '''
