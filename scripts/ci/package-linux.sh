266ce8537ecc435be4afcbfdd826da3c755ed1e8
dekarrin <dekarrin.irl@gmail.com>
2024-10-06 09:32:22 -0500

    apparently i spelt it wrong the whole time

--- scripts/ci/package-linux.sh
+++ scripts/ci/package-linux.sh
@@ -11,7 +11,7 @@ echo 'DEBUG "file builds/$LINUX_RELEASE_NAME"'
 file "builds/$LINUX_RELEASE_NAME"
 echo 'DEBUG "file dist/$LINUX_RELEASE_NAME/mtgdb"'
 file "dist/$LINUX_RELEASE_NAME/mtgdb" || echo "File Not Found; Expected"
-mv "builds/$LIUNX_RELEASE_NAME" "dist/$LINUX_RELEASE_NAME/mtgdb"
+mv "builds/$LINUX_RELEASE_NAME" "dist/$LINUX_RELEASE_NAME/mtgdb"
 echo "Archive Contents IMMEDIATE MOVE:"
 ls -la "dist/$LINUX_RELEASE_NAME"
 echo 'DEBUG "file dist/$LINUX_RELEASE_NAME/mtgdb"'
