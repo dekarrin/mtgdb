73cabffd38b43b3a277bda1d5ff3f88bbe081981
dekarrin <dekarrin.irl@gmail.com>
2024-10-06 09:51:56 -0500

    can linux raise dist by one level

--- scripts/ci/package-linux.sh
+++ scripts/ci/package-linux.sh
@@ -5,9 +5,9 @@ set -ex
 mkdir -p "dist/$LINUX_RELEASE_NAME"
 mv "builds/$LINUX_RELEASE_NAME" "dist/$LINUX_RELEASE_NAME/mtgdb"
 cp -R common-assets/* "dist/$LINUX_RELEASE_NAME"
-cd "dist/$LINUX_RELEASE_NAME"
+cd dist
 echo "Archive Contents:"
-ls -la
-tar czf "../$LINUX_RELEASE_NAME.tar.gz" ./*
-cd ../..
+ls -la "$LINUX_RELEASE_NAME"
+tar czvf "$LINUX_RELEASE_NAME.tar.gz" -C "$LINUX_RELEASE_NAME" .
+cd ..
 mv "dist/$LINUX_RELEASE_NAME.tar.gz" archives/
