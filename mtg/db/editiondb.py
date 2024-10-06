db73607e9ef6bc3385e556b89d823b135216f97b
Rebecca Nelson <dekarrin.irl@gmail.com>
2024-10-06 18:37:20 -0500

    Update editiondb.py - Add get all editions

--- mtg/db/editiondb.py
+++ mtg/db/editiondb.py
@@ -1,5 +1,7 @@
 from . import util
 
+from ..types import Edition
+
 
 def find(db_filename, name_filter=''):
     con = util.connect(db_filename)
@@ -15,6 +17,24 @@ def find(db_filename, name_filter=''):
     
     return data
 
+
+def get_all(db_filename: str) -> dict[str, Edition]:
+    d = dict()
+    
+    con = util.connect(db_filename)
+    cur = con.cursor()
+    
+    for r in cur.execute(sql_get_all):
+        ed = Edition(code=r[0], name=r[1], release_date=datetime.date.fromisoformat(r[2])}
+        d[ed.code] = ed
+    
+    con.close()
+    
+    return d
+
+sql_get_all = '''
+SELECT code, name, release_date FROM editions;
+'''
 
 sql_find_editions = '''
 SELECT code, name, release_date FROM editions WHERE name LIKE "%" || ? || "%";
